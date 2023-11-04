e233ee75505d820234c574cbaf27ef61e6b43bc5
kushagrakatiha <kushagrakatiha123@gmail.com>
2023-11-04 19:25:16 +0530

    setuser created

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,7 +1,35 @@
+const User = require("../models/userModel");
+
 const home = (req, res) => {
     res.send("<h1>Hello from the controller!</h1>");
 }
 
+const setDetails = (req, res) => {
+    const {name, username, email, password} = req.body
+    try{
+
+        if(!name || !username || !email || !password) throw new Error `Please provide all details`
+
+        const user = new User({
+            name,
+            username,
+            email,
+            password
+        })
+        
+        user.save()
+
+    }catch(err){
+        res.status(400).json({
+            success: false, 
+            message: err.message
+        })
+    }
+}
+const getDetails = () => {}
+
 module.exports = {
+    setDetails,
+    getDetails,
     home
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { home } = require('../controllers/userControllers');
+const { home, getDetails, setDetails } = require('../controllers/userControllers');
 const router = express.Router();
 
 router.get("/", home)
+router.post("/setdetails", setDetails)
+router.get("/getdetails", getDetails)
 
 
 module.exports = router;
